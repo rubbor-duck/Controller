eb80cf91b23b20a0d3c2ae5eccd8ddfaa63b0493
rubbor-duck <nelson.jordan10@hotmail.com>
2026-05-16 14:46:07 -0600

    working on buttons and analog inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
-/*
- * Reads a potentiometer on pin 34 and maps the reading to the X axis
- *
- * Potentiometers can be noisy, so the sketch can take multiple samples to average out the readings
- */
-
 #include <Arduino.h>
 #include <BleGamepad.h>
 
 
 
 BleGamepadConfiguration config;
-BleGamepad bleGamepad("Crusty Controller", "DeezNutz", 100);
+BleGamepad bleGamepad("Crusty Controller", "Jordan The Grand", 100);
 
 void buttonTask(void);
+void pinModeSetup(void);
 
+// TODO change GPIO placeholder number to actual numbers
 // Button mapping from GPIO port
 #define BTN_X_PIN       GPIO_NUM_4
 #define BTN_O_PIN       GPIO_NUM_5
@@ -30,6 +26,14 @@ void buttonTask(void);
 #define BTN_LB_PIN      GPIO_NUM_16
 #define BTN_RB_PIN      GPIO_NUM_17
 
+// Analog Mapping from GPIO port
+#define ANALOG_LX_PIN   GPIO_NUM_18
+#define ANALOG_LY_PIN   GPIO_NUM_19
+#define ANALOG_RX_PIN   GPIO_NUM_20
+#define ANALOG_RY_PIN   GPIO_NUM_21
+#define ANALOG_LT_PIN   GPIO_NUM_26
+#define ANALOG_RT_PIN   GPIO_NUM_27
+
 // saves the buttons into an array for cleaner use
 // Button order in array:
 // X, O, SQR, TRI, DUP, DRIGHT, DLEFT, DDOWN, START, SELECT, L3, R3, LB, RB
@@ -44,12 +48,20 @@ const uint8_t BTN_PINS[] = {
 // size of array / size of each element in array = # of elements (32 byte / 1 byte = # of elements)
 const int NUM_BUTTONS = sizeof(BTN_PINS) / sizeof(BTN_PINS[0]);
 
+// saves the analog inputs into an array for cleaner use
+const uint8_t ANALOG_PINS[] = {
+      ANALOG_LX_PIN, ANALOG_LY_PIN, ANALOG_RX_PIN, ANALOG_RY_PIN,
+      ANALOG_LT_PIN, ANALOG_RT_PIN
+};
+
+const int NUM_ANALOG = sizeof(ANALOG_PINS) / sizeof(ANALOG_PINS[0]);
+
 uint32_t lastState = 0;
 
 
 // function to read all the buttons, check if the last read is different than the current read,
 // and then sends the report to the computer if the buttons pressed are different
-void buttonTask(void *pvParameters)
+void buttonTask(void)
 {
   // creates the clock and has a lastWake so the function will sleep between pollings
   const TickType_t pollInterval = pdMS_TO_TICKS(10); //10ms or 100Hz polling rate
@@ -68,12 +80,24 @@ void buttonTask(void *pvParameters)
   }
 }
 
+void pinModeSetup(void)
+{
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    pinMode(BTN_PINS[i], INPUT_PULLUP);
+  }
 
+  for (int i = 0; i < NUM_ANALOG_INPUTS; i++)
+  {
+
+  }
+}
 
 void setup() {
   bleGamepad.begin();
   config.setControllerType(CONTROLLER_TYPE_GAMEPAD);
   bleGamepad.begin(&config);
+
 
 }
 
